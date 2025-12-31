cb12d33d0a05df9b47d9bb8a9aa50450fea2864d
akbarob <akbarbadmus07@gmail.com>
2025-12-31 17:35:51 +0100

    feat: Replace search input with a search button and add a notification indicator to the bell icon in the navbar.

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -15,17 +15,20 @@ const Navbar = ({ breadcrumbs }: NavbarProps) => {
             {/* Right Section: Search, Notifications, Profile */}
             <div className='flex items-center gap-4'>
                 {/* Search Bar */}
-                <div className='relative flex items-center'>
+                {/* <div className='relative flex items-center'>
                     <Search className='absolute left-3.5 w-5 h-5 text-[#41414380]' />
                     <input
                         type='text'
                         placeholder='Quick search here...'
                         className='h-12 w-full max-w-[235px] bg-[#FCFCFD] pl-11 pr-4 rounded-full text-sm outline-none placeholder:text-[#41414380] focus:ring-1 focus:ring-gray-200 transition-all'
                     />
-                </div>
-
+                </div> */}
+                <button className='w-12 h-12 flex items-center justify-center rounded-full bg-[#FCFCFD] transition-colors text-[#0A0A0C] border-[#DFE1E7] border'>
+                    <Search className='w-5 h-5' />
+                </button>
                 {/* Notifications */}
-                <button className='w-12 h-12 flex items-center justify-center rounded-full bg-[#FCFCFD] transition-colors text-[#0A0A0C]'>
+                <button className='w-12 h-12 flex relative items-center justify-center rounded-full bg-[#FCFCFD] transition-colors text-[#0A0A0C] border-[#DFE1E7] border'>
+                    <div className='bg-[#DF1C41] rounded-full absolute right-3 top-2 w-3 h-3' />
                     <Bell className='w-5 h-5' />
                 </button>
 
