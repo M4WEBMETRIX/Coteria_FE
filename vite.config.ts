609623f868d1b57acc4d75f60ca1b293f52a1486
akbarob <akbarbadmus07@gmail.com>
2026-01-13 21:29:03 +0100

    chore: configuration updates for PWA

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,20 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
   server: {
     port: 3000,
     allowedHosts: ["careless-neighbor.outray.app"],
